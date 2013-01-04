03231223a39423336096eabdfd35bb1326bea720
Aidi Stan <aidistan@live.cn>
2013-01-04 20:36:23 +0800

    v0.0.3.3
    Page待整理

---
+++ SandBox/SandBox/Pages/PayForPlantPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SandBox.Pages
+{
+    /// <summary>
+    /// PayForPlantPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class PayForPlantPage : Page
+    {
+        public PayForPlantPage()
+        {
+            InitializeComponent();
+        }
+		
+		private void Next_Click(object sender, RoutedEventArgs e)
+		{
+			(App.Current as App).action.Update();
+		}
+    }
+}
